10052fa69ce85bc6e71e8c8ac2e4d4b74727642d
Dmitry Fleish <DXF0530@corp.aeroscout.com>
2019-03-26 19:05:00 +0200

    create modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'material',
+    pathMatch: 'full'
+  },
+  {
+    path: 'material',
+    loadChildren: './material/material.module#MaterialModule',
+  },
+  {
+    path: 'prime',
+    loadChildren: './prime/prime.module#PrimeModule',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/prime/prime-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrimeComponent } from './prime.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PrimeComponent,
+    pathMatch: 'full'
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PrimeRoutingModule { }
